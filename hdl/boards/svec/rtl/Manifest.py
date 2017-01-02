dafbb7932439076a60664c8ddb97bd3c5e6ebc97
Tom Levens <tom.levens@cern.ch>
2017-01-02 16:44:10 +0100

    Repository cleanup
    Signed-off-by: Tom Levens <tom.levens@cern.ch>

--- hdl/boards/svec/rtl/Manifest.py
+++
@@ -1,9 +0,0 @@
-files=["IRQ_Generator_Top.vhd",
-"IRQ_generator.vhd",
-"ram_8bits.vhd",
-"spram.vhd",
-"TOP_LEVEL.vhd",
-"WB_Bridge.vhd",
-"xwb_ram.vhd",
-"wishbone_pkg.vhd",
-"genram_pkg.vhd"];
--- hdl/boards/svec/sim/testbench/run.do
+++
@@ -1,5 +0,0 @@
-vlib work
-make
-vsim -t 1ps -L unisim -c work.vme64x_tb
-do wave1.do
-run 100us
