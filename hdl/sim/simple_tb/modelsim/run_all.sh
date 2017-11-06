756ef1a8cca463fda1485e6257c259870971a8ac
Dimitris Lampridis <Dimitris.Lampridis@cern.ch>
2017-11-06 14:06:39 +0100

    sim: drop "bash-ism" from run_all.sh

--- hdl/sim/simple_tb/modelsim/run_all.sh
+++ hdl/sim/simple_tb/modelsim/run_all.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 set -e
 
-function check_log()
-{
-  if grep -F '# ** ' sim.log | grep -E -v 'Note|Warning'; then
-    echo "Simulation failed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    exit 1
-  fi
-}
-
 for i in 1 2 3 4 5 6 7 8 9; do
   echo
   echo "Scenario $i"
   vsim -gg_scenario=$i -c -do "run 5us; quit" top_tb | tee sim.log
-  check_log
+  # check log
+  if grep -F '# ** ' sim.log | grep -E -v 'Note|Warning'; then
+    echo "Simulation failed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit 1
+  fi
 done
 
 echo "OK!"
