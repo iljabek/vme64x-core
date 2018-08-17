0ae24a25ae24cc9f9830c502a7881795238b54c2
Tristan Gingold <tristan.gingold@cern.ch>
2018-08-17 16:09:26 +0200

    run_all.sh: strenghten.

--- hdl/testbench/simple_tb/modelsim/run_all.sh
+++ hdl/testbench/simple_tb/modelsim/run_all.sh
@@ -10,6 +10,10 @@ for i in 1 2 3 4 5 6 7 8 9; do
     echo "Simulation failed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     exit 1
   fi
+  if ! grep -F "# ** Note: end of simulation" sim.log; then
+    echo "Simulation failed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit 1
+  fi
 done
 
 echo "OK!"
